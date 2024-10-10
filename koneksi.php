2c5976e6d7083503d480fe0d9b23123b0368ce06
Fadillah222 <fadillahmuhammad222@gmail.com>
2024-10-10 10:23:30 +0700

    first commit

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,13 @@
 <?php 
-$koneksi = mysqli_connect("localhost","root","","spprizki");
- 
-// Check connection
-if (mysqli_connect_errno()){
-	echo "Koneksi database gagal : " . mysqli_connect_error();
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$koneksi = mysqli_connect("localhost", "root", "", "spprizki");
+
+// Cek koneksi
+if (mysqli_connect_errno()) {
+    echo "Koneksi database gagal: " . mysqli_connect_error();
+} else {
+    echo "Koneksi berhasil!";
 }
- 
 ?>
